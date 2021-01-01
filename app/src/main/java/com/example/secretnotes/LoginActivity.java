ea9336295a46c3d4818926843ba0b9512ad261ee
HotwheelH <haiderkhan343@gmail.com>
2021-01-01 15:38:54 +0000

    progress with button sending me to another activity

--- app/src/main/java/com/example/secretnotes/LoginActivity.java
+++ app/src/main/java/com/example/secretnotes/LoginActivity.java
@@ -6,29 +6,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+
     private Button signupButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        signupButton = findViewById(R.id.signupButton);
+
+        signupButton = (Button) findViewById(R.id.signupButton);
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                moveToRegisterActivity();
+            public void onClick(View view) {
+                openSignUpActivity();
+            }
+        });
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
             }
         });
 
     }
 
-    private void moveToRegisterActivity() {
+    public void openSignUpActivity() {
         Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
         startActivity(intent);
     }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 
 }
--- app/src/main/java/com/example/secretnotes/SignupActivity.java
+++ app/src/main/java/com/example/secretnotes/SignupActivity.java
@@ -2,13 +2,32 @@ package com.example.secretnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        textView = (TextView) findViewById(R.id.ExistingAccount);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
