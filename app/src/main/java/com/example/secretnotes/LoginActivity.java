63e3f60b4d7ff87709d24a0ab067da9471d60abf
HotwheelH <haiderkhan343@gmail.com>
2021-01-01 14:27:50 +0000

    changed names of activities

---
+++ app/src/main/java/com/example/secretnotes/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.secretnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button signupButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signupButton = findViewById(R.id.signupButton);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToRegisterActivity();
+            }
+        });
+
+    }
+
+    private void moveToRegisterActivity() {
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/com/example/secretnotes/SignupActivity.java
+++ app/src/main/java/com/example/secretnotes/SignupActivity.java
@@ -2,33 +2,13 @@ package com.example.secretnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class SignupActivity extends AppCompatActivity {
-
-    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        signupButton = findViewById(R.id.signupButton);
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moveToRegisterActivity();
-            }
-        });
-
+        setContentView(R.layout.activity_signup);
     }
-
-    private void moveToRegisterActivity() {
-        Intent intent = new Intent(SignupActivity.this, activity_register_user.class);
-        startActivity(intent);
-    }
-
 }
