27cd8d1be3b24734c5c469191c1421c49a2b03ab
HotwheelH <haiderkhan343@gmail.com>
2021-01-06 13:52:23 +0000

    link ForgotPasswordActivity with CodeEntryActivity upon clicking the send code button

--- app/src/main/java/com/example/secretnotes/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/secretnotes/ForgotPasswordActivity.java
@@ -2,13 +2,31 @@ package com.example.secretnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private Button sendCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        sendCode = (Button) findViewById(R.id.send_code);
+        sendCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCodeEntryActivity();
+            }
+        });
+    }
+
+    public void openCodeEntryActivity() {
+        Intent intent = new Intent(ForgotPasswordActivity.this, CodeEntry.class);
+        startActivity(intent);
     }
 }
