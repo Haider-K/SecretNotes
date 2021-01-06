501bca8f7fca1d8943005354ab625cc0d900e9dd
HotwheelH <haiderkhan343@gmail.com>
2021-01-06 13:20:54 +0000

    created code entry page activity and layout

---
+++ app/src/main/java/com/example/secretnotes/CodeEntry.java
@@ -0,0 +1,14 @@
+package com.example.secretnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CodeEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_entry);
+    }
+}
